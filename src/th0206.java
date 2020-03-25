2fb63756d76adcc02101a6de567ada809d9af31d
Karry Hane <62607456+karryhane@users.noreply.github.com>
2020-03-25 22:30:05 +0900

    Delete th0206.java

--- src/th0206.java
+++
@@ -1,9 +0,0 @@
-
-public class th0206 {
-//체온값 36.5도로 초기화된 상수 bodyTemp를 double형으로 선언하시오
-	public static void main(String[] args) {
-		double bodyTemp = 36.5;
-		System.out.println(bodyTemp);
-	}
-
-}
