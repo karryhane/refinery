f1666fb8f145f2064869f89435caecaf48d4b2b8
karryhane <xiamidz@gmail.com>
2020-03-25 21:23:30 +0900

    ...

---
+++ src/th0202.java
@@ -0,0 +1,15 @@
+
+public class th0202 {
+
+	public static void main(String[] args) {
+//에러가 안 났다
+			int i ;
+			int j ;
+			i = 10;
+			j = 20;
+			
+			System.out.println(i+j);
+	
+	}
+
+}
---
+++ src/th0210.java
@@ -0,0 +1,12 @@
+
+public class th0210 {
+	//조건연산자 이용하여 이프문 한 문장으로 변환
+		public static void main(String[] args) {
+			int n = 7;
+			
+			int a = (n > 5)?n:5;
+	{
+			System.out.println("n: " + a);
+		}
+
+	}
